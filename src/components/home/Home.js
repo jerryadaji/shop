76299d9b0bbb5fd8eba10cd417c60e61c738fbcf
Jerry <32514732+jerryadaji@users.noreply.github.com>
2021-06-29 12:00:11 +0100

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,23 +1,10 @@
 import React from "react";
-
-import { useSelector } from "react-redux";
-import { useFirestoreConnect } from "react-redux-firebase";
-
 import Title from "../Title";
 
 const Home = () => {
-  useFirestoreConnect([{
-    collection: 'products'
-  }])
-
-  const products = useSelector((state) => state.firestore.ordered.products)
-
-  console.log(products)
-
   return (
     <div className="container">
       <Title>Home</Title>
-      {products && <ul>{products.map(product => <li key={product.id}>{product.name}</li>)}</ul>}
     </div>
   );
 };
